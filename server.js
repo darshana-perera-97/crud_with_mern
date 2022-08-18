66205232388e9bf46006723b385e2c07d03830bd
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-18 07:05:53 +0530

    port 3001 assigned for backend running

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const port = 3001;
+
+
+
+app.listen(port, function () {
+    console.log("Express is running");
+  });
+  
