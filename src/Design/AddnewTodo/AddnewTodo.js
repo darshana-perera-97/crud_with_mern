9dbec165f3ea7a443b8d2570f83c4fbc32f949a9
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-18 07:22:38 +0530

    add todo created

--- src/Design/AddnewTodo/AddnewTodo.js
+++ src/Design/AddnewTodo/AddnewTodo.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default function AddnewTodo() {
   return (
-    <div>AddnewTodo</div>
-  )
+    <div style={{ padding: "20px",textAlign:"center" }}>
+      <input
+        // onChange={handleChange}
+        name="title"
+        // value={item.title}
+        placeholder="title"
+      ></input>
+      <input
+        // onChange={handleChange}
+        name="description"
+        // value={item.description}
+        placeholder="description"
+      ></input>
+      <button
+      //    onClick={addItem}
+      >
+        ADD ITEM
+      </button>
+    </div>
+  );
 }
